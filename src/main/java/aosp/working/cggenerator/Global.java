978475421c7df510cfe2308a538d83494e00845b
skyleaworlder <870033938@qq.com>
2022-04-16 14:47:34 +0800

    config: use Global to read config

--- src/main/java/aosp/working/cggenerator/Global.java
+++ src/main/java/aosp/working/cggenerator/Global.java
@@ -3,8 +3,21 @@ package aosp.working.cggenerator;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Global {
     public static Logger logger = Logger.getLogger(Global.class);
+    public static Properties props = new Properties();
+
+    public static void initConfig() {
+        try (FileInputStream fis = new FileInputStream("io.properties")) {
+            Global.props.load(fis);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void initLogger() {
         PropertyConfigurator.configure("log4j.properties");
